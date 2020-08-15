fa0d449c75a005b6badcead2c381e61c7c2ad3ec
astricus <astricus@hotmail.com>
2020-08-15 11:35:17 +0300

    test commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import './App.css';
-
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
