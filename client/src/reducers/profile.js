9f71e1f0038b2a37d6a820dc088082d0f61204f0
astricus <astricus@hotmail.com>
2020-08-15 15:05:04 +0300

    added dashboard, profile, spinner

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,36 @@
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
+
+export default function (state = initialState, action) {
+  const { type, paylaod } = action;
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: paylaod,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: paylaod,
+        loading: false,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
